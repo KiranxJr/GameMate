25edb0e740279218095d9bde3f0a7ffaa9f26e9e
Kiran Peter <110444515+KiranBeo@users.noreply.github.com>
2025-06-16 11:20:15 +0530

    Initial Commit

---
+++ GameMate/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  GameMate
+//
+//  Created by admin on 16/06/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ GameMate/GameMateApp.swift
@@ -0,0 +1,17 @@
+//
+//  GameMateApp.swift
+//  GameMate
+//
+//  Created by admin on 16/06/25.
+//
+
+import SwiftUI
+
+@main
+struct GameMateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
