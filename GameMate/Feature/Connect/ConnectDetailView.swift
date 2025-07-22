8dc6df3dd2796ff6cbf3dba0e6673fee9d5c43ba
Kiran Peter <kiran.peter@beo.in>
2025-07-22 17:23:12 +0530

    Connect detail page

---
+++ GameMate/Feature/Connect/ConnectDetailView.swift
@@ -0,0 +1,18 @@
+//
+//  ConnectDetailView.swift
+//  GameMate
+//
+//  Created by admin on 21/07/25.
+//
+
+import SwiftUI
+
+struct ConnectDetailView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ConnectDetailView()
+}
