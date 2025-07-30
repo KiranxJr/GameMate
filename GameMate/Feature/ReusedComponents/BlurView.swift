208c0d006e0758b301ee8b7c73d17886e32e7ceb
Kiran Peter <kiran.peter@beo.in>
2025-07-30 16:57:33 +0530

    Detail page almost complete

---
+++ GameMate/Feature/ReusedComponents/BlurView.swift
@@ -0,0 +1,18 @@
+//
+//  BlurView.swift
+//  GameMate
+//
+//  Created by admin on 29/07/25.
+//
+
+import SwiftUI
+
+struct BlurView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    BlurView()
+}
